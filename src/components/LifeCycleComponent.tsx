2070cb2ac4984ec3f60efb7e4275188ea168f2b0
Tony <tony.js@zoho.eu>
2020-11-07 13:05:46 -0300

    feat: add description of cycles in react component

--- src/components/LifeCycleComponent.tsx
+++ src/components/LifeCycleComponent.tsx
@@ -20,14 +20,33 @@ class LifeCycle extends Component<LifeCycleProps, LifeCycleState> {
     this.state = { mode: props.mode }
   }
 
+  /**
+   * descontinuado futurament removido do react
+   * UNSAFE_componentWillMount
+   * Executado antes do componente ser montado ( render executado pela primeira vez )
+   */
+  componentWillMount() { console.log( 'componentWillMount' ) }
+  UNSAFE_componentWillMount() { console.log( 'UNSAFE_componentWillMount' ) }
+
   // public static getDerivedStateFromProps( props: LifeCycleProps, state: LifeCycleState ): Partial<LifeCycleState> {
   //   console.log( 'getDerivedStateFromProps' )
   //   return { mode: state.mode }
   // }
 
+  /**
+   * descontinuado futurament removido do react
+   * UNSAFE_componentWillReceiveProps
+   * Antes de receber novas propriedades.
+   * Apenas é executado quando o pai ( parent ) for atualizado.
+   */
   componentWillReceiveProps( nextProps: LifeCycleProps ) { console.log( 'componentWillReceiveProps' ) }
   UNSAFE_componentWillReceiveProps( nextProps: LifeCycleProps ) { console.log( 'UNSAFE_componentWillReceiveProps' ) }
 
+  /**
+   * Executando antes de receber novas propriedades ou um novo estado.
+   * Responsavel por prevenir renderizações desnecessarias com valores boolean.
+   * Recebe as novas propriedades e o novo estado.
+   */
   shouldComponentUpdate( nextProps: LifeCycleProps, nextState: LifeCycleState ) {
     // console.log( this.props, nextProps )
     // console.log( this.state, nextState )
@@ -35,9 +54,11 @@ class LifeCycle extends Component<LifeCycleProps, LifeCycleState> {
     return true
   }
 
-  componentWillMount() { console.log( 'componentWillMount' ) }
-  UNSAFE_componentWillMount() { console.log( 'UNSAFE_componentWillMount' ) }
-
+  /**
+   * descontinuado futurament removido do react
+   * UNSAFE_componentWillUpdate
+   * Executado antes do componente sofrer atualização ( antes do render ser executado )
+   */
   componentWillUpdate() { console.log( 'componentWillUpdate' ) }
   UNSAFE_componentWillUpdate() { console.log( 'UNSAFE_componentWillUpdate' ) }
 
@@ -53,11 +74,20 @@ class LifeCycle extends Component<LifeCycleProps, LifeCycleState> {
       </div>
     )
   }
-
+  /**
+   * Executado depois que o componente for montado ( depois que o render set executado pela primeira vez )
+   */
   componentDidMount() { console.log( 'componentDidMount' ) }
 
+  /**
+   * Executado depois do componente sofrer atualização ( depois do render ser executado )
+   */
   componentDidUpdate() { console.log( 'componentDidUpdate' ) }
 
+  /**
+   * Executado toda vez que o componente for desmontado.
+   * **** Removido da arvore do react.
+   */
   componentWillUnmount() { console.log( 'componentWillUnmount' ) }
 }
 
